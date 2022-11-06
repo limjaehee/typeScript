331faa083b2972b6bfa3800a0f79d1e49c21ebf0
jaehee <rhak39@gmail.com>
2022-11-06 22:54:56 +0900

    타입스크립트 기본타입

--- day3/app.js
+++ day3/app.js
@@ -1,3 +1,31 @@
 "use strict";
-let strTxt = "테스트";
-console.log(strTxt);
+{
+    let str = "text";
+}
+{
+    let arr = [1, 2, 3];
+    let arrGeneric = [1, 2, 3];
+}
+{
+    let arr = ["hello", 1];
+    console.log(arr[0].concat("!"));
+}
+{
+    let Animals;
+    (function (Animals) {
+        Animals[Animals["cat"] = 0] = "cat";
+        Animals[Animals["dog"] = 1] = "dog";
+        Animals[Animals["rabbit"] = 2] = "rabbit";
+    })(Animals || (Animals = {}));
+    let zoo = Animals[0];
+}
+{
+    let undefinedType = undefined;
+    // Type 'null' is not assignable to type 'void'.
+}
+{
+    // 이 함수는 절대 함수의 끝까지 실행되지 않는다는 의미
+    function neverEnd() {
+        while (true) { }
+    }
+}
--- day3/app.ts
+++ day3/app.ts
@@ -1,2 +1,30 @@
-let strTxt: string = "테스트";
-console.log(strTxt);
+{
+    let str: string = "text";
+}
+{
+    let arr: number[] = [1, 2, 3];
+    let arrGeneric: Array<number> = [1, 2, 3];
+}
+{
+    let arr: [string, number] = ["hello", 1];
+    console.log(arr[0].concat("!"));
+}
+{
+    enum Animals {
+        cat = 0,
+        dog,
+        rabbit,
+    }
+
+    let zoo = Animals[0];
+}
+{
+    let undefinedType: void = undefined;
+    // Type 'null' is not assignable to type 'void'.
+}
+{
+    // 이 함수는 절대 함수의 끝까지 실행되지 않는다는 의미
+    function neverEnd(): never {
+        while (true) {}
+    }
+}
