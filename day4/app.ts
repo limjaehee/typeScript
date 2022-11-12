2d341353c65e0566fda7253de9e6a92dab3421c5
jaehee <rhak39@gmail.com>
2022-11-12 21:02:32 +0900

    타입스크립트 함수 공부

---
+++ day4/app.ts
@@ -0,0 +1,80 @@
+{
+    function sum(a: number, b: number): number {
+        console.log(`sum ${a + b}`);
+        return a + b;
+    }
+
+    sum(10, 20);
+
+    //An argument for 'b' was not provided.
+    //sum(10);
+
+    // Argument of type 'string' is not assignable to parameter of type 'number'.
+    //sum(10, "a");
+
+    //Argument of type 'null' is not assignable to parameter of type 'number'.
+    // sum(10, null);
+
+    /// error, too many parameters
+    // sum(10, 20, 30);
+}
+{
+    function notReturnSum(a: number, b: number): void {
+        console.log(`notReturnSum ${a + b}`);
+    }
+
+    notReturnSum(10, 20);
+}
+{
+    function sum(a: number, b?: number): number {
+        if (b) {
+            return a + b;
+        }
+        return a;
+    }
+    sum(10);
+}
+{
+    function sum(a: number, b = 100): number {
+        console.log(`initializationSum ${a + b}`);
+        return a + b;
+    }
+
+    sum(10);
+    sum(10, undefined);
+    // sum(10, null);
+}
+{
+    function sum(a: number, ...nums: number[]): number {
+        let total: number = 0;
+        // nums.forEach((num) => (total += num));
+
+        for (let num of nums) {
+            total += num;
+        }
+
+        return a + total;
+    }
+
+    console.log(sum(10, 30, 40, 50));
+}
+{
+    let deck = {
+        suits: ["hearts", "spades", "clubs", "diamonds"],
+        cards: Array(52),
+        createCardPicker: function () {
+            // NOTE: 아랫줄은 화살표 함수로써, 'this'를 이곳에서 캡처할 수 있도록 합니다
+            return () => {
+                let pickedCard = Math.floor(Math.random() * 52);
+                let pickedSuit = Math.floor(pickedCard / 13);
+
+                return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
+            };
+        },
+    };
+
+    let cardPicker = deck.createCardPicker();
+    let pickedCard = cardPicker();
+
+    console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
+}
